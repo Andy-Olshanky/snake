0e3bd6b9cbeae889d7482cd283a4580ab10fc67c
Andy Olshansky <105177790+Andy-Olshanky@users.noreply.github.com>
2024-01-20 16:34:29 -0500

    Delete target directory

--- target/debug/deps/snake-c933b9161e3a95d2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\andyo\RustProjects\snake\target\debug\deps\libsnake-c933b9161e3a95d2.rmeta: src\main.rs
-
-C:\Users\andyo\RustProjects\snake\target\debug\deps\snake-c933b9161e3a95d2.d: src\main.rs
-
-src\main.rs:

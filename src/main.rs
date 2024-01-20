31352831849a29bc9ca91c2012c91b4a8d1dbd10
Andy <olshansa@gmail.com>
2024-01-20 17:10:49 -0500

    implemented positioning and basic directions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
+use ggez::{
+    event, graphics,
+    input::keyboard::{KeyCode, KeyInput},
+    Context, GameResult,
+};
+use oorandom::Rand32;
+use std::collections::VecDeque;
+
+const GRID_SIZE: (i16, i16) = (30, 20);
+const GRID_CELL_SIZE: (i16, i16) = (32, 32);
+const SCREEN_SIZE: (f32, f32) = (
+    GRID_SIZE.0 as f32 * GRID_CELL_SIZE.0 as f32,
+    GRID_SIZE.1 as f32 * GRID_CELL_SIZE.1 as f32,
+);
+const DESIRED_FPS: u32 = 10;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct GridPosition {
+    x: i16,
+    y: i16,
+}
+
+impl GridPosition {
+    pub fn new(x: i16, y: i16) -> Self {
+        GridPosition { x, y }
+    }
+
+    pub fn random(rng: &mut Rand32, max_x: i16, max_y: i16) -> Self {
+        // You can use .into() to directly convert the i16 tuple to GridPosition
+        // since From<i16, i16> is implemented into GridPosition below
+        (
+            rng.rand_range(0..(max_x as u32)) as i16,
+            rng.rand_range(0..(max_y as u32)) as i16,
+        )
+            .into()
+    }
+
+    // using rem_euclid here since % can give a negative remainder which we don't want
+    // rem_euclid only gives positive values (aka what it actually should be...)
+    pub fn new_from_move(pos: GridPosition, dir: Direction) -> Self {
+        match dir {
+            Direction::Up => GridPosition::new(pos.x, (pos.y - 1).rem_euclid(GRID_SIZE.1)),
+            Direction::Down => GridPosition::new(pos.x, (pos.y + 1).rem_euclid(GRID_SIZE.1)),
+            Direction::Left => GridPosition::new((pos.x - 1).rem_euclid(GRID_SIZE.0), pos.y),
+            Direction::Right => GridPosition::new((pos.x + 1).rem_euclid(GRID_SIZE.0), pos.y),
+        }
+    }
+}
+
+// Allows us to easily go from GridPosition to the graphics display
+impl From<GridPosition> for graphics::Rect {
+    fn from(pos: GridPosition) -> Self {
+        graphics::Rect::new_i32(
+            pos.x as i32 * GRID_CELL_SIZE.0 as i32,
+            pos.y as i32 * GRID_CELL_SIZE.1 as i32,
+            GRID_CELL_SIZE.0 as i32,
+            GRID_CELL_SIZE.1 as i32,
+        )
+    }
+}
+
+// This allows us to go from (i16, i16) to GridPosition easily
+impl From<(i16, i16)> for GridPosition {
+    fn from(pos: (i16, i16)) -> Self {
+        GridPosition { x: pos.0, y: pos.1 }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 fn main() {
     println!("Hello, world!");
 }
